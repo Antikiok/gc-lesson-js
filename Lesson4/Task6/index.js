178e865cd6198dee05d2ba395294167f2134775b
Antikiok <antikiok@gmail.com>
2021-08-09 01:19:20 +0300

    Do Lesson4 Task6

---
+++ Lesson4/Task6/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable prefer-const */
+const start = 4;
+const end = 12;
+let result = 0;
+
+// eslint-disable-next-line no-shadow
+for (let start = 4; start <= end; start += 1) {
+  if (start % 5 === 0) {
+    console.log(start);
+  }
+  if (start % 2 === 0 && start % 4 !== 0) {
+    result += start;
+  }
+  if (start % 3 === 0) {
+    result -= start;
+  }
+  if (start % 4 === 0) {
+    result *= start;
+  }
+}
