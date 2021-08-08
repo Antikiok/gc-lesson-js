ad40701c5b831b078e97b8e1a50da74da23a2fc1
Antikiok <antikiok@gmail.com>
2021-08-09 01:39:00 +0300

    some fix

--- Lesson4/Task7/index.js
+++ Lesson4/Task7/index.js
@@ -5,10 +5,5 @@ for (let i = 0; i <= x; i += 1) {
   if (i % 2 === 1) {
     sum += i;
     console.log('Found');
-    if (sum * 5 > 5000) {
-      console.log('Bigger');
-    } else {
-      console.log('Smaller or equal');
-    }
   }
 }
