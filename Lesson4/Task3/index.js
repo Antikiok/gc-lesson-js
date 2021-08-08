171941f2f9349069b13425ee9ffe20133ed4c8a2
Antikiok <antikiok@gmail.com>
2021-08-09 00:49:47 +0300

    Do Task5 Lesson5

--- Lesson4/Task3/index.js
+++ Lesson4/Task3/index.js
@@ -1,7 +1,6 @@
-//put your code here
-for (let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 5; i += 1) {
   console.log('=> ' + i + ' <=');
-  for (let a = 1; a <= 5; a++) {
+  for (let a = 1; a <= 5; a += 1) {
     console.log(i + ' x ' + a + ' = ' + i * a);
   }
 }
---
+++ Lesson4/Task5/index.js
@@ -0,0 +1,7 @@
+const n = 10;
+// eslint-disable-next-line prefer-const
+let sum = 0;
+for (let i = 0; i < n; i += 1) {
+  sum += 1;
+  console.log(sum);
+}
