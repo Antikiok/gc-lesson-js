ff34b18a2caa8997fc1cb0c559ee263339877f95
Antikiok <antikiok@gmail.com>
2021-08-16 03:30:20 +0300

    do Task9

---
+++ Lesson6/Task9/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable for-direction */
+function reverseArray(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  const copyArray = arr.slice();
+  const revArr = [];
+  for (let i = copyArray.length - 1; i >= 0; i -= 1) {
+    revArr.push(copyArray[i]);
+  }
+  return revArr;
+}
+
+console.log(reverseArray([11, 22, 33, 44]));
