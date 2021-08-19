225c16de79f8c7c6a50ef10ab9690864c37d0cd2
Antikiok <antikiok@gmail.com>
2021-08-19 17:29:11 +0300

    Do Lesson 8 Task1

---
+++ Lesson8/Task1/index.js
@@ -0,0 +1,12 @@
+const array = ['a', 17.1, 'John Doe'];
+const transformToObject = (arr) => {
+  const object = {};
+  arr.forEach((element) => {
+    object[element] = element;
+  });
+  return object;
+};
+console.log(transformToObject(array));
+
+// examples
+transformToObject(['a', 17.1, 'John Doe']); // ==> { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe' }
