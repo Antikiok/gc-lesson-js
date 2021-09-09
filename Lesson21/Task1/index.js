d1c363a59d7e279037142bce811671832ba9ed10
Antikiok <antikiok@gmail.com>
2021-09-09 11:03:06 +0300

    Do Lesson21 Task1

---
+++ Lesson21/Task1/index.js
@@ -0,0 +1,41 @@
+const listElem = document.querySelector('.list');
+
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const renderTasks = tasksList => {
+  listElem.innerHTML = '';
+
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }, index) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.setAttribute('data-id', index);
+      checkbox.checked = done;
+
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
+
+renderTasks(tasks);
+
+const createBtnElement = document.querySelector('.create-task-btn');
+const inputElement = document.querySelector('.task-input');
