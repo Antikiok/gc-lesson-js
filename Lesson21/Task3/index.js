442402df5aee40d6e866b818ec539c99959942b1
Antikiok <antikiok@gmail.com>
2021-09-09 13:59:23 +0300

    Do Lesson 21 Task3

---
+++ Lesson21/Task3/index.js
@@ -0,0 +1,15 @@
+export const getItemList = () => {
+  const elementList = document.querySelectorAll('.technology');
+  console.dir(elementList);
+  return elementList;
+};
+
+export const getItemsArray = () => {
+  const elementsArray = Array.from(document.querySelectorAll('.tool'));
+  console.dir(elementsArray);
+  return elementsArray;
+};
+
+// test data
+getItemList();
+getItemsArray();
