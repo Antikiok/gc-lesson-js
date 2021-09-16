b810be193b16486d84caf0fcd4cae6c714535f43
Antikiok <antikiok@gmail.com>
2021-09-16 18:30:58 +0300

    fix

--- Lesson27/Task3/script/index.js
+++ Lesson27/Task3/script/index.js
@@ -5,3 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
   renderTasks();
   initTodoListHandlers();
 });
+const onStorageChange = event => {
+  if (event.key === 'tasksList') {
+    renderTasks();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
